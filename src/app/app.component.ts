8a96ce47683dba635e924f4f470c34e6e2a977e2
Lóránt Pintér <lorant.pinter@gmail.com>
2018-09-09 23:23:40 +0200

    Add file upload

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+import { ReadFile, ReadMode } from 'ngx-file-helpers';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +9,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'raiffeisen-ynab-importer';
+  readMode = ReadMode.arrayBuffer;
+
+  constructor() {}
+
+  onFileLoad(file) {
+    var text = new TextDecoder('windows-1250').decode(file.content);
+    console.log(text);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { FileHelpersModule } from 'ngx-file-helpers';
+
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -11,6 +13,7 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    FileHelpersModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
