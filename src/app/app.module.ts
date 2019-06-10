3f10f88ca39515ebacd762c99891707b3393959c
Lóránt Pintér <lorant.pinter@gmail.com>
2019-06-10 15:37:59 +0200

    Import CSV

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule } from '@angular/material/button';
 import { MatTabsModule } from '@angular/material/tabs';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatTableModule } from '@angular/material/table';
 
 import { FileHelpersModule } from 'ngx-file-helpers';
 
@@ -22,11 +23,13 @@ import { AppComponent } from './app.component';
     MatButtonModule,
     MatTabsModule,
     MatToolbarModule,
+    MatTableModule,
   ],
   exports: [
     MatButtonModule,
     MatTabsModule,
     MatToolbarModule,
+    MatTableModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
